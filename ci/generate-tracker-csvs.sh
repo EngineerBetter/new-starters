8de39389c1ed27e26700e0d81fbdae194f62b4c8
Daniel Jones <daniel.jones@engineerbetter.com>
2021-06-01 12:59:10 +0100

    generate csvs in pipeline

---
+++ ci/generate-tracker-csvs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source_dir=${1}
+dest_dir=${2}
+
+echo "Looking in ${source_dir} for .prolific files"
+for yaml in ${source_dir}/*.prolific
+do
+  echo "Processing $yaml"
+  without_extension="$(basename $yaml .prolific)"
+  csv_filepath="${dest_dir}${without_extension}.csv"
+  prolific ${yaml} > "${csv_filepath}"
+  echo "Wrote ${csv_filepath}"
+done
+
+echo "Done."
