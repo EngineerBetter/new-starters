3665c9020f745fb9e3f2dd2972ed4de21899ebe2
Sam Colson <sam.colson@engineerbetter.com>
2021-09-02 10:58:21 +0100

    Make json compatible with API

--- ci/convert-csv-to-json.sh
+++ ci/convert-csv-to-json.sh
@@ -2,10 +2,18 @@
 
 set -euo pipefail
 
-go get github.com/jiangmiao/csv2json
+go install github.com/jiangmiao/csv2json@latest
 
 for csv in csvs/*.csv
 do
     json_file="jsons/$(basename "$csv" .csv).json"
-    csv2json < "$csv" > "$json_file"
+    jq_query='[.[] '\
+'| .["name"] = .Title '\
+'| .["story_type"] = .Type '\
+'| .["description"] = .Description '\
+'| .["labels"] = .Labels '\
+'| del(.Title, .Type, .Description, .Labels)] '\
+'| map(.labels |= split(","))'
+
+    csv2json < "$csv" | jq "$jq_query" > "$json_file"
 done
