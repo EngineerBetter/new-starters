65a5ce31d4c75c3befc58a5003a7581dc07cfee1
Tom Godkin <tom.godkin@engineerbetter.com>
2021-09-02 14:06:08 +0100

    Clean up story creation logs

--- ci/create-stories.sh
+++ ci/create-stories.sh
@@ -5,14 +5,18 @@ set -euo pipefail
 : "${TRACKER_TOKEN:?Variable not set or empty}"
 : "${TRACKER_PROJECT_ID:?Variable not set or empty}"
 
-api_url="https://www.pivotaltracker.com/services/v5"
+api_url='https://www.pivotaltracker.com/services/v5'
 
 while IFS= read -r line; do
+  echo -n "Creating story \"$( jq -r '.name' <<< "$line" )\"..."
+
   curl \
     --request POST \
     --header "X-TrackerToken: $TRACKER_TOKEN" \
-    --header "Content-Type: application/json" \
+    --header 'Content-Type: application/json' \
     --data "$line" \
-    "$api_url/projects/$TRACKER_PROJECT_ID/stories"  
-done <<< "$(cat jsons/*.json)"
+    "$api_url/projects/$TRACKER_PROJECT_ID/stories" \
+    1> /dev/null
 
+  echo " done!"
+done <<< "$( cat jsons/*.json )"
