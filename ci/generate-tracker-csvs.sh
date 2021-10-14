d7a0003d22b53a8946fe0cf8c27c8817a2aed775
Richard Pryce <richard.pryce@engineerbetter.com>
2021-10-14 10:29:04 +0100

    Fix generating stories with spaces in filename
    Signed-off-by: Sam Colson <sam.colson@engineerbetter.com>

--- ci/generate-tracker-csvs.sh
+++ ci/generate-tracker-csvs.sh
@@ -5,12 +5,27 @@ set -euo pipefail
 source_dir=${1}
 dest_dir=${2}
 
+find ${source_dir}*.prolific -name '* *' | while read fname
+
+do
+        new_fname=`echo $fname | tr " " "_"`
+
+        if [ -e $new_fname ]
+        then
+                echo "File $new_fname already exists. Not replacing $fname"
+        else
+                echo "Creating new file $new_fname to replace $fname"
+                mv "$fname" $new_fname
+        fi
+done
+
 echo "Looking in ${source_dir} for .prolific files"
-for yaml in ${source_dir}/*.prolific; do
-  echo "Processing $yaml"
-  without_extension="$(basename "$yaml" .prolific)"
+for file in ${source_dir}*.prolific; do
+
+  echo "Processing $file"
+  without_extension="$(basename "$file" .prolific)"
   csv_filepath="${dest_dir}${without_extension}.csv"
-  prolific "$yaml" > "${csv_filepath}"
+  prolific "$file" > "${csv_filepath}"
   echo "Wrote ${csv_filepath}"
 done
 
